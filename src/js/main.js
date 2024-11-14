b819eed661f01c5dcc2ba6ec8d8ac55981e60a46
julia-ranzi <juliasara.ranzi@studentfr.ch>
2024-11-14 11:02:43 +0100

    animation scroll horizontal

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,25 @@
-import { gsap } from "gsap";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".container-carousel", {
-  xPercent: -67,
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".horizontal-scroll",
-    start: "top top",
-    end: "+=300%",
-    scrub: 1,
-    pin: true,
-  },
+window.addEventListener("DOMContentLoaded", function () {
+  //ajout du plugin ScrollTrigger
+  gsap.registerPlugin(ScrollTrigger);
+
+  const screenWidth = window.screen.width;
+  const containerWidth =
+    document.querySelector(".scroll-container").offsetWidth;
+  console.log(screenWidth, containerWidth);
+  gsap.to(".scroll-container", {
+    x: containerWidth - screenWidth,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".horizontal-scroll",
+      start: "top top",
+      end: "+=300%",
+      scrub: 1,
+      pin: true,
+    },
+  });
 });
