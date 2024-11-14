d605536b51c7d4294e41f3d96d9ba36c3d675fcd
julia-ranzi <juliasara.ranzi@studentfr.ch>
2024-11-14 12:10:13 +0100

    animation 2 et ajustements généraux

--- src/js/main.js
+++ src/js/main.js
@@ -11,15 +11,45 @@ window.addEventListener("DOMContentLoaded", function () {
   const containerWidth =
     document.querySelector(".scroll-container").offsetWidth;
   console.log(screenWidth, containerWidth);
-  gsap.to(".scroll-container", {
-    x: containerWidth - screenWidth,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".horizontal-scroll",
-      start: "top top",
-      end: "+=300%",
-      scrub: 1,
-      pin: true,
-    },
-  });
+
+  gsap
+    .timeline()
+    .to(".scroll-container", {
+      x: containerWidth - screenWidth,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".horizontal-scroll",
+        start: "top top",
+        end: "+=300%",
+        scrub: 1,
+        pin: true,
+      },
+    })
+    .from(".green-1", {
+      left: "0",
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".green-1",
+        start: "top bottom",
+        scrub: true,
+      },
+    })
+    .from(".green-2", {
+      right: "0",
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".green-2",
+        start: "top bottom",
+        scrub: true,
+      },
+    })
+    .from(".green-3", {
+      left: "0",
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".green-3",
+        start: "top bottom",
+        scrub: true,
+      },
+    });
 });
