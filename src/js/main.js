72f9eee0048c6c1f6b18de96fb2ac2c33413d6cb
julia-ranzi <juliasara.ranzi@studentfr.ch>
2024-11-28 15:35:03 +0100

    ajout commentaires

--- src/js/main.js
+++ src/js/main.js
@@ -53,3 +53,37 @@ window.addEventListener("DOMContentLoaded", function () {
       },
     });
 });
+
+//explication des éléments scroll horizontal
+
+//le gsap va animer l'élément grâce à la classe .container
+
+//le xPercent déplace horizontalement l'élément de -67% (calcul fait par rapport à la largeur de l'écran)
+
+//ease: "none" fait que il y a pas de variation de vitesse (la vitesse dépend de notre scroll).
+
+//Dans scrollTrigger on a configuré le déclenchement de l'animation.
+
+//Trigger: ".horizontal-scroll", veut dire que l'animation commence lors de la class .horizontal-scroll.
+
+//Start: "top top" est là pour que l'animation commence quand le haut de l'élément .horizontal-scroll est en haut de l'écran.
+
+//End: "+=300%" fait que l'animation se finit quand on a scrollé sur une distance qui équivaut à 3 fois la taille de l'écran (de la fenêtre).
+
+//Scrub:1, ajoute de la fluidité au scroll.
+
+//Pour fixer l'élément dans la fenêtre et donner l'impression qu'on scroll horizontalement nous avons ajouté : pin: true.
+
+// explications des éléments d'apparition au scroll
+
+// Faire apparaitre les différents éléments en modifiant leur position et leur opacité quand ils entrent dans l'écran de l'utilisateur. Les éléments sont tout d'abord placés en dehors de l'écran, puis viennent se regrouper vers le milieu de celui-ci en se positionnant en cascade.
+
+//Dans scrollTrigger on a configuré le déclenchement de l'animation.
+
+//Les éléments 1 et 3 arrivent depuis la gauche, d'où le paramètre "left" et l'élément 2 arrive depuis la droite, d'ou le "right".
+
+//Le "start: "top bottom"" signifie que l'animation commencera lorsque le haut des éléments "box" atteignent le bas de la fenêtre de visualisation.
+
+// "scrub:true" synchronise l'animation avec le défilement, donc elle avance ou recule en fonction du déplacement de la page.
+
+// ces paramètres sont répétés sur chacune des 3 box.
